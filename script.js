14c018a235611979478d75cf7f63efb28b9d2762
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-02-09 02:21:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+$(document).ready(() => {
+  // Function to get the current timer value from local storage
+  const getSavedTime = (clockId) => {
+    const savedTime = localStorage.getItem(`timerValue_${clockId}`);
+    return savedTime ? parseInt(savedTime) : 600; // Default to 10 minutes if no value is found
+  };
+
+  // Function to get the current state of isWhite from local storage
+  const getIsWhite = () => {
+    const savedIsWhite = localStorage.getItem("isWhite");
+    return savedIsWhite ? JSON.parse(savedIsWhite) : false; // Default to false if no value is found
+  };
+
+  // Initialize FlipClock instances with the saved time
+  const whiteClock = new FlipClock($("#white_timer"), getSavedTime("white"), {
+    clockFace: "MinuteCounter",
+    autoStart: false,
+    countdown: true,
+    callbacks: {
+      stop: () => {},
+      interval: () => {
+        // Save the current timer value to local storage every second
+        localStorage.setItem("timerValue_white", whiteClock.getTime().time);
+        localStorage.setItem("isWhite", JSON.stringify(isWhite)); // Save isWhite state
+      },
+    },
+  });
+
+  const blackClock = new FlipClock($("#black_timer"), getSavedTime("black"), {
+    clockFace: "MinuteCounter",
+    autoStart: false,
+    countdown: true,
+    callbacks: {
+      stop: () => {},
+      interval: () => {
+        // Save the current timer value to local storage every second
+        localStorage.setItem("timerValue_black", blackClock.getTime().time);
+        localStorage.setItem("isWhite", JSON.stringify(isWhite)); // Save isWhite state
+      },
+    },
+  });
+
+  let isWhite = getIsWhite();
+
+  $(document).on("keydown", (event) => {
+    if (event.key === " ") {
+      isWhite = !isWhite;
+      // Check if space bar is pressed
+      if (isWhite) {
+        whiteClock.stop();
+        blackClock.start();
+      } else {
+        blackClock.stop();
+        whiteClock.start();
+      }
+    } else if (event.key === "Enter") {
+      // Check if the Enter key is pressed
+      // Reset both FlipClocks to the initial time (10 minutes)
+      whiteClock.stop();
+      blackClock.stop();
+      whiteClock.setTime(600);
+      blackClock.setTime(600);
+      localStorage.removeItem("timerValue_white"); // Clear stored value when resetting
+      localStorage.removeItem("timerValue_black"); // Clear stored value when resetting
+      localStorage.setItem("isWhite", JSON.stringify(false)); // Set isWhite to false in local storage
+      isWhite = false; // Reset isWhite to false
+    }
+  });
+});
