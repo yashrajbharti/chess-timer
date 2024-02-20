16ed2287bd42fc3f2c8dd8b26389219f131e3f4d
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-02-21 00:18:25 +0530

    feat: blitz Mode

--- scriptBlitz.js
+++ scriptBlitz.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
   // Function to get the current timer value from local storage
   const getSavedTime = (clockId) => {
-    const savedTime = localStorage.getItem(`timerValue_${clockId}`);
-    return savedTime ? parseInt(savedTime) : 600; // Default to 10 minutes if no value is found
+    const savedTime = localStorage.getItem(`timerValueBlitz_${clockId}`);
+    return savedTime ? parseInt(savedTime) : 180; // Default to 10 minutes if no value is found
   };
 
   // Function to get the current state of isWhite from local storage
   const getIsWhite = () => {
-    const savedIsWhite = localStorage.getItem("isWhite");
+    const savedIsWhite = localStorage.getItem("isWhiteBlitz");
     return savedIsWhite ? JSON.parse(savedIsWhite) : false; // Default to false if no value is found
   };
 
@@ -36,8 +36,11 @@ $(document).ready(() => {
       },
       interval: () => {
         // Save the current timer value to local storage every second
-        localStorage.setItem("timerValue_white", whiteClock.getTime().time);
-        localStorage.setItem("isWhite", JSON.stringify(isWhite)); // Save isWhite state
+        localStorage.setItem(
+          "timerValueBlitz_white",
+          whiteClock.getTime().time
+        );
+        localStorage.setItem("isWhiteBlitz", JSON.stringify(isWhite)); // Save isWhite state
       },
     },
   });
@@ -61,8 +64,11 @@ $(document).ready(() => {
       },
       interval: () => {
         // Save the current timer value to local storage every second
-        localStorage.setItem("timerValue_black", blackClock.getTime().time);
-        localStorage.setItem("isWhite", JSON.stringify(isWhite)); // Save isWhite state
+        localStorage.setItem(
+          "timerValueBlitz_black",
+          blackClock.getTime().time
+        );
+        localStorage.setItem("isWhiteBlitz", JSON.stringify(isWhite)); // Save isWhite state
       },
     },
   });
@@ -79,11 +85,13 @@ $(document).ready(() => {
       // Check if space bar is pressed
       if (isWhite) {
         whiteClock.stop();
+        whiteClock.setTime(whiteClock.getTime().time + 10);
         white_timer?.classList?.remove("active_timer");
         blackClock.start();
         black_timer.classList.add("active_timer");
       } else {
         blackClock.stop();
+        blackClock.setTime(blackClock.getTime().time + 10);
         black_timer?.classList?.remove("active_timer");
         whiteClock.start();
         white_timer.classList.add("active_timer");
@@ -91,28 +99,28 @@ $(document).ready(() => {
     } else if (event.key === "Enter") {
       // Check if the Enter key is pressed
       // Reset both FlipClocks to the initial time (10 minutes)
-      whiteClock.setTime(600);
+      whiteClock.setTime(180);
       whiteClock.stop();
-      blackClock.setTime(600);
+      blackClock.setTime(180);
       blackClock.stop();
       modal.style.display = "none";
-      localStorage.removeItem("timerValue_white"); // Clear stored value when resetting
-      localStorage.removeItem("timerValue_black"); // Clear stored value when resetting
-      localStorage.setItem("isWhite", JSON.stringify(false)); // Set isWhite to false in local storage
+      localStorage.removeItem("timerValueBlitz_white"); // Clear stored value when resetting
+      localStorage.removeItem("timerValueBlitz_black"); // Clear stored value when resetting
+      localStorage.setItem("isWhiteBlitz", JSON.stringify(false)); // Set isWhite to false in local storage
       isWhite = false; // Reset isWhite to false
     } else if (event.key === "Shift" || event.key === "p") {
       window.location.reload();
     } else if (event.key === "b") {
-      window.location.href = "./blitz.html";
+      window.location.href = "./";
     }
   });
   restart.addEventListener("click", () => {
     modal.style.display = "none";
-    whiteClock.setTime(600);
-    blackClock.setTime(600);
-    localStorage.removeItem("timerValue_white"); // Clear stored value when resetting
-    localStorage.removeItem("timerValue_black"); // Clear stored value when resetting
-    localStorage.setItem("isWhite", JSON.stringify(false)); // Set isWhite to false in local storage
+    whiteClock.setTime(180);
+    blackClock.setTime(180);
+    localStorage.removeItem("timerValueBlitz_white"); // Clear stored value when resetting
+    localStorage.removeItem("timerValueBlitz_black"); // Clear stored value when resetting
+    localStorage.setItem("isWhiteBlitz", JSON.stringify(false)); // Set isWhite to false in local storage
     isWhite = false; // Reset isWhite to false
   });
 });
